13c982acb0fa202d55e20d78de30a8fd5eece288
nickdepaolo <79952881+nickdepaolo@users.noreply.github.com>
2021-04-20 20:19:26 -0400

    8:20

--- local-conditions/src/App.js
+++ local-conditions/src/App.js
@@ -1,39 +1,15 @@
 import React, {useState} from 'react';
 // import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
-import Weather from "./weather/Weather"
+import UpdateWeather from "./weather/Weather"
 
 function App()
 {
-    const [lon, setLon] = useState("");
-    const [lat, setLat] = useState("");
-    
-    
-    const locate = (props) => {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        setLat(position.coords.latitude);
-        setLon(position.coords.longitude);
-      });
-      updateLocation();
-    }
-    
-    const updateLocation = () => {
-      localStorage.setItem('lon', lon);
-      localStorage.setItem('lat', `${lat}`);
-      console.log(`${lat}`);
-    }
-    
-    locate()
-    
-
-
-      
-
 
     return(
       <>
       <h1>sccdcdc</h1>
-      <Weather/>
+      <UpdateWeather/>
       </>
     );
 
--- local-conditions/src/weather/Weather.js
+++ local-conditions/src/weather/Weather.js
@@ -1,22 +1,61 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import { Container, Col } from 'reactstrap';
 
 const UpdateWeather = (event) => {
-    const [lat, setLat] = useState(localStorage.getItem('lat'));
-    const [lon, setLon] = useState(localStorage.getItem('lon'));
+    const [lat, setLat] = useState('');
+    const [lon, setLon] = useState('');
+    const [temp, setTemp] = useState('');
     
-    fetch(`http://api.openweathermap.org/data/2.5/weather?lat={${lat}}&lon={${lon}}&appid={b8e15013886caf192c16ed2b4c284e3d}`, {
-        method: 'GET',
-        headers: new Headers({
-            'Content-Type': 'application/json'
+   
+    const Locate = () => {
+        
+        navigator.geolocation.getCurrentPosition(function(position) {
+          setLat(position.coords.latitude);
+          setLon(position.coords.longitude);
         })
-    }).then((res) => res.json())
+       
+       
+    }
+    
+    Locate();
+
+    const fetchWeather = () => {
+        
+        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=b8e15013886caf192c16ed2b4c284e3d`, {
+            method: 'GET'
+           
+        }).then((res) => res.json())
+        .then((props) => {
+            console.log(props)
+            setTemp(props.main)
+            console.log(`${temp}`)
+        })
+    }
+    useEffect(() => {
+        fetchWeather();
+    }, [])
+
+
+
+    const weatherMapper = (props) => {  console.log(props)
+        return(() => {
+            return(
+                <h1>55555</h1>
+            )
+        })
+    }
+  
+    
+    
     
 
 
     return(
-        <>
-        <h1>weather</h1>
-        </>
+        <Container>
+            <Col>
+               
+            </Col>
+        </Container>
     );
 
 }
